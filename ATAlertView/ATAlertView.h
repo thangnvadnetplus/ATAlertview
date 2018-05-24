158a191547b174e18ff45cb2a6c7a90145f51461
Apple <apple@applenomacbook-pro.local>
2018-05-24 16:00:31 +0700

    Initial Commit

---
+++ ATAlertView/ATAlertView.h
@@ -0,0 +1,19 @@
+//
+//  ATAlertView.h
+//  ATAlertView
+//
+//  Created by Thang Nguyen on 2018/05/24.
+//  Copyright © 2018 Thang Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ATAlertView.
+FOUNDATION_EXPORT double ATAlertViewVersionNumber;
+
+//! Project version string for ATAlertView.
+FOUNDATION_EXPORT const unsigned char ATAlertViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ATAlertView/PublicHeader.h>
+
+
